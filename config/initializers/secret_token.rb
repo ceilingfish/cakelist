c19a85df090ba6251fbb2381412b5d27bba8624d
twilliams <thomas.williams@dubitlimited.com>
2011-02-23 23:28:29 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeddingMenu::Application.config.secret_token = 'b7fb2bb3028fcdfc7646ca91d1fe49b255ff96387c78b12c1e89513b917e8789d26977d964ea95594839e4ae029487aa853d17560238b441a592fa9acdfd6326'
---
+++ db/migrate/20110223232550_create_menu_items.rb
@@ -0,0 +1,13 @@
+class CreateMenuItems < ActiveRecord::Migration
+  def self.up
+    create_table :menu_items do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :menu_items
+  end
+end
