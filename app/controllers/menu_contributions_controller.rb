439725061f53060784be3aa31603aba368f45793
Tom Parrish <tom.parrish@crispthinking.com>
2011-12-18 18:48:45 +0000

    Add a mechanism to store the contributions for each guest

--- app/controllers/menu_contributions_controller.rb
+++ app/controllers/menu_contributions_controller.rb
@@ -1,2 +1,20 @@
 class MenuContributionsController < ApplicationController
+	
+	def create
+		@menu_contribution = MenuContribution.new(params[:menu_contribution])
+		@menu_item = MenuItem.where(:name.matches => params[:menu_item][:name].gsub(/%/,"")).first
+		
+		@menu_item = MenuItem.create(params[:menu_item]) if @menu_item.nil?
+		
+		@menu_contribution.menu_item = @menu_item
+		
+		if @menu_contribution.save!
+			flash[:thankyou] = "You wonderful, wonderful person!"
+			redirect_to menu_items_url
+		else
+			flash[:error] = "Oh, something went wrong!"
+			redirect_to new_menu_items_url
+		end
+		
+	end
 end
--- app/models/menu_contribution.rb
+++ app/models/menu_contribution.rb
@@ -1,3 +1,6 @@
 class MenuContribution < ActiveRecord::Base
-	validates_inclusion_of :type, :in => %w( paid made )
+	#validates_inclusion_of :type, :in => ["paid", "made"]
+	
+	has_one :menu_item
+	has_one :guest
 end
